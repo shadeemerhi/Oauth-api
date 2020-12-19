b5bcdc14d7c0366ecfbc2c434311ba270da0a876
Shadee Merhi <shadmerhi@gmail.com>
2020-12-18 23:57:10 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const Express = require('express');
+const app = Express();
+const BodyParser = require('body-parser');
+const PORT = 8080;
+
+// Express Configuration
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json());
+app.use(Express.static('public'));
+
+// Sample GET route
+app.get('/api/data', (req, res) => {
+  console.log('at the endpoint');
+  res.send('Sending back to client');
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${PORT}`);
+});
